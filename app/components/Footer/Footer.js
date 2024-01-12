fcee4fd5c6f8f5a5b75508de328d38021e547444
rhmmmmmm <prasham.gautam@gmail.com>
2024-01-12 11:29:29 +0530

    footer update

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import '../../globals.css'
 const Footer = () => {
   return (
     <div className='text-center bottom-10 p-10'> 
-        <span>&copy; rhmmmmmm 2022-23</span> | <span>All rights reserved.</span>
+        <span>&copy; rhmmmmmm 2022-24</span> | <span>All rights reserved.</span>
     </div>
   )
 }
